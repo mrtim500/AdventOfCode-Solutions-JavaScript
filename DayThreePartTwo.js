d253c469a49b30793965357b0c2ee2d1cd9dfa43
mrtim500 <pindakaas500@gmail.com>
2015-12-10 10:58:04 +0100

    solution day three part two

--- DayThreePartTwo.js
+++ DayThreePartTwo.js
@@ -1,41 +1,65 @@
 var fs = require('fs');
 
-var map = [ 0, 0];
+var map1 = [ 0, 0];
+var map2 = [ 0, 0];
 var housesVisited = [];
-var count = 1;
+var count = 0;
+var countFalse = 0;
+var santa;
+
+var addToMap = function(move, aMap){
+
+        switch( move ) {
+            case '<':
+            	aMap[1] -= 1;
+                break;
+            case '>':
+            	aMap[1] += 1;
+                break;
+            case '^':
+            	aMap[0] += 1;
+                break;
+            case 'v':
+            	aMap[0] -= 1;
+                break;
+                            };
+
+                 
+        if((housesVisited.indexOf(aMap.toString()) === -1 ) ){
+            housesVisited.push(aMap.toString());
+            count += 1;
+
+        } else {
+            countFalse += 1;
+        };
+      	 
+};	
+
+
 
 fs.readFile('DayThreeInput.txt', function( err, data) {
     if (err) throw err;
 
-    housesVisited.push(map.toString());
+    housesVisited.push(map1.toString()); //startpunt
+    count += 1;
     
     for( i = 0; i < data.length; i++ ){
         var currentMove = data.slice(i, i + 1).toString();
-
-        switch( currentMove ) {
-            case '<':
-                map[1] -= 1;
-                break;
-            case '>':
-                map[1] += 1;
-                break;
-            case '^':
-                map[0] += 1;
-                break;
-            case 'v':
-                map[0] -= 1;
-                break;
-                            };
-
-        if(housesVisited.indexOf(map.toString()) === -1 ){
-            housesVisited.push(map.toString());
-            count += 1;
-        };
-    }
+        
+        if( santa == true ){
+        	addToMap(currentMove, map1);
+        	santa = false;
+        } else {
+        	addToMap(currentMove, map2);
+        	santa = true;
+        }
+        
+    };
 
     console.log(count);
-    
+    console.log(countFalse);    
 });
+
 
 
 
